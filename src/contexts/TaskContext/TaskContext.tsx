ae7b5d3d0cbc00544c1ad09836c959a9bad1f4bc
Kaironn3 <jonathas_oliveira17@hotmail.com>
2025-07-31 10:46:09 -0300

    feat: task reducer hook

--- src/contexts/TaskContext/TaskContext.tsx
+++ src/contexts/TaskContext/TaskContext.tsx
@@ -1,12 +1,13 @@
-import { createContext, type SetStateAction } from "react";
+import { createContext } from "react";
 import type { TaskStateModel } from "../../models/TaskStateModel";
 import { initialTaskState } from "./initialTaskState";
+import type { TaskActionModel } from "./taskActions";
 
 type TaskContextProps = {
   state: TaskStateModel;
-  setState: React.Dispatch<SetStateAction<TaskStateModel>>;
+  dispatch: React.Dispatch<TaskActionModel>;
 };
 
-const initialContextValue = { state: initialTaskState, setState: () => {} };
+const initialContextValue = { state: initialTaskState, dispatch: () => {} };
 
 export const TaskContext = createContext<TaskContextProps>(initialContextValue);
---
+++ src/contexts/TaskContext/taskActions.ts
@@ -0,0 +1,26 @@
+import type { TaskModel } from "../../models/TaskModel";
+
+export const TaskActionTypes = {
+  START_TASK: 'START_TASK',
+  INTERRUPT_TASK: 'INTERRUPT_TASK',
+  RESET_STATE: 'RESET_STATE',
+} as const;
+
+export type TaskActionTypes = typeof TaskActionTypes[keyof typeof TaskActionTypes];
+
+export type TaskActionsWithPayload =
+  | {
+      type: typeof TaskActionTypes.START_TASK;
+      payload: TaskModel;
+    }
+  | {
+      type: typeof TaskActionTypes.INTERRUPT_TASK;
+    };
+
+export type TaskActionsWithoutPayload = {
+  type: typeof TaskActionTypes.RESET_STATE;
+};
+
+export type TaskActionModel =
+  | TaskActionsWithPayload
+  | TaskActionsWithoutPayload;
