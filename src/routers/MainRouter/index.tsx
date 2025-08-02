894ff3f4cb53a37eb1cf7c7915ab2abfebebdfb8
kaironn2 <jonathas_oliveira17@hotmail.com>
2025-08-02 01:07:16 -0300

    feat: settings router + timer adjust

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -1,11 +1,10 @@
-import { Route, useLocation } from "react-router";
-import { Routes } from "react-router";
-import { BrowserRouter } from "react-router";
-import { Home } from "../../pages/Home";
-import { AboutPomodoro } from "../../pages/AboutPomodoro";
-import { NotFound } from "../../pages/NotFound";
-import { useEffect } from "react";
-import { History } from "../../pages/History";
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
+import { AboutPomodoro } from '../../pages/AboutPomodoro';
+import { NotFound } from '../../pages/NotFound';
+import { Home } from '../../pages/Home';
+import { useEffect } from 'react';
+import { History } from '../../pages/History';
+import { Settings } from '../../pages/Settings';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -19,16 +18,15 @@ function ScrollToTop() {
 
 export function MainRouter() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about-pomodoro" element={<AboutPomodoro />} />
-          <Route path="/history" element={<History />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <ScrollToTop />
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/history/' element={<History />} />
+        <Route path='/settings/' element={<Settings />} />
+        <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <ScrollToTop />
+    </BrowserRouter>
   );
 }
